f44576ee706dc46b9110d8b3599c2cadaa43e9dc
Ahmed <waterydesert.developer@gmail.com>
2022-10-26 20:31:22 +0530

    update

--- NodeTutorialSwiftUiCode/NodeTutorialSwiftUiCodeApp.swift
+++ NodeTutorialSwiftUiCode/NodeTutorialSwiftUiCodeApp.swift
@@ -11,11 +11,7 @@ import SwiftUI
 struct NodeTutorialSwiftUiCodeApp: App {
     var body: some Scene {
         WindowGroup {
-            // When loading todo from mongodb or memory
             TodoHome()
-            
-            // When loading todo with auth and JWT
-            MyApp()
         }
     }
 }
