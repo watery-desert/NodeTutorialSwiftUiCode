99c1db27aa7fe0e20e2605b3111b9c2aa64a05ac
Ahmed <waterydesert.developer@gmail.com>
2022-10-26 19:07:25 +0530

    Initial Commit

---
+++ NodeTutorialSwiftUiCode/NodeTutorialSwiftUiCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodeTutorialSwiftUiCodeApp.swift
+//  NodeTutorialSwiftUiCode
+//
+//  Created by Ahmed on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct NodeTutorialSwiftUiCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
